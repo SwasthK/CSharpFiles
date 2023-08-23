b0f87d40276a67e19c1be3adfeb9c6096941c7bd
Swasthik K <220981@sdmcujire.in>
2023-08-23 22:58:19 +0530

    Prime_Or_Not.cs Added
    Program to Check Whether a Number is Prime or Not .If it is Prime Display Prime , else Display Non-Prime and also Display Its Factors .

---
+++ Prime_Or_Not/Prime_Or_Not.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Prime_Or_Not
+{   
+    public class Program
+    {
+        public void primeOrNot(int n)
+        {
+            int flag=0,c=0;
+            if(n==1)
+            {
+                Console.WriteLine($"{n} is a Non-Prime Number\nFactor : {n}\nNumber of Factors : 1");
+                return;
+            }
+            for(int i=2;i<=n/2;i++)
+            {
+                if(n%i==0)
+                {
+                    if(flag==0)
+                    {
+                        Console.Write($"******************\n{n} is a Non-Prime Number\nFactors : 1\t{i}\t");
+                        flag=1;
+                    }
+                    else
+                    {
+                        Console.Write($"{i}\t");
+                    }
+                    c++;
+                }
+            }
+            if(flag==1)
+            {
+                 Console.Write($"{n}\nNumber of Factors : {c+2}");
+            }
+            else
+            {
+                Console.Write($"******************\n{n} is a Prime Number");
+            }  
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the Number : ");
+            int n = int.Parse(Console.ReadLine());
+            Program p=new();
+            p.primeOrNot(n);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CSharpFiles
  Contains Basic C# Program Files
+1."PrimeorNot" folder consists Prime_Or_Not file : 
+Program to Check Whether a Number is Prime or Not .If it is Prime Display Prime , else Display Non-Prime and also Display Its Factors .
